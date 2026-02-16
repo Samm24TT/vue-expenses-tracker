d8dfd2b24bc567cc0d8f7de7ff05bd306b0c2af7
Sam24 <aclian4321@gmial.com>
2026-02-16 15:33:51 +0800

    Initial commit

---
+++ vue-expense-tracker/src/App.vue
@@ -0,0 +1,96 @@
+
+<template>
+    <Header />
+    <div class="container">
+        <Balance :total="+total"/>
+        <IncomeExpenses :income="+income" :expenses="+expenses"/>
+        <TransactionList :transactions="transactions" @transaction-deleted="handleTransactionDeleted"/>
+        <AddTransaction @-transaction-submitted="handleTransactionSubmitted"/>
+    </div>
+</template>
+
+<script setup>
+    import Balance from './components/Balance.vue';
+    import Header from './components/Header.vue';
+    import IncomeExpenses from './components/IncomeExpenses.vue';
+    import TransactionList from './components/TransactionList.vue';
+    import AddTransaction from './components/AddTransaction.vue';
+
+    import { useToast } from 'vue-toastification';
+
+    import { ref, computed, onMounted } from 'vue';
+
+    onMounted(() => {
+        const savedTransactions = JSON.parse(localStorage.getItem('transactions'));
+
+        if (savedTransactions) {
+            transactions.value = savedTransactions;
+        }
+    });
+
+    const toast = useToast();
+
+    const transactions = ref([]);
+
+    //Get Total
+    const total = computed (() => {
+        return transactions.value.reduce((acc,  transaction) => {
+            return acc + transaction.amount;
+        }, 0)
+    })
+
+    //Get Income
+    const income = computed (() => {
+        return transactions.value
+        .filter((transaction) => transaction.amount > 0) 
+        .reduce((acc,  transaction) => {
+            return acc + transaction.amount;
+        }, 0)
+        .toFixed(2);
+    });
+
+    //Get Expenses
+    const expenses = computed (() => {
+        return transactions.value
+        .filter((transaction) => transaction.amount < 0) 
+        .reduce((acc,  transaction) => {
+            return acc + transaction.amount;
+        }, 0)
+        .toFixed(2);
+    });
+
+    //Add transaction
+    const handleTransactionSubmitted = (transactionData) => {
+        transactions.value.push({
+            id: generateUniqueId(),
+            text: transactionData.text,
+            amount: transactionData.amount
+        });
+
+        saveTransactionsToLocalStorage();
+
+        toast.success('Transaction added')
+    };
+
+    //Generate Unique ID
+    const generateUniqueId = () => {
+        return Math.floor(Math.random() * 1000000);
+    }
+
+    //Deleted transaction
+    const handleTransactionDeleted = (id) => {
+        transactions.value = transactions.value.filter(
+            (transaction) => transaction.id !== id
+        );
+
+        saveTransactionsToLocalStorage();
+
+        toast.success('Transaction deleted')
+    }
+
+    //Saved to local storage
+    const saveTransactionsToLocalStorage = () => {
+        localStorage.setItem('transaction', JSON.stringify(transactions.value))
+    }
+
+</script>
---
+++ vue-expense-tracker/src/components/Balance.vue
@@ -0,0 +1,15 @@
+<template>
+    <h4>Your balance</h4>
+    <p id="balance">${{ total }}</p>
+</template>
+
+<script setup> 
+    import { defineProps } from 'vue';
+
+    const props = defineProps ({
+        total: {
+            type: Number,
+            requried: true,
+        }
+    });
+</script>
